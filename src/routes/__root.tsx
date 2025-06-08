e81803e6c99417a55625dc9967d7cf4f8b5671c1
Darcy Robson <17976794+drobson03@users.noreply.github.com>
2025-06-08 19:50:11 +1000

    fix: add missing meta tags

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,6 +9,15 @@ import styles from "~/styles/app.css?url";
 
 export const Route = createRootRoute({
   head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+    ],
     links: [{ rel: "stylesheet", href: styles }],
   }),
   component: RootComponent,
