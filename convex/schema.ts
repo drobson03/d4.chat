457498b8fed8ce944c2d464683f849c747237f6b
Darcy Robson <17976794+drobson03@users.noreply.github.com>
2025-06-09 20:07:34 +1000

    chore: add chats schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,8 +1,104 @@
-import { defineSchema } from "convex/server";
+import { defineSchema, defineTable } from "convex/server";
 import { authTables } from "@convex-dev/auth/server";
+import { v } from "convex/values";
+
+const TextUIPart = v.object({
+  type: v.literal("text"),
+  text: v.string(),
+});
+
+const ReasoningUIPart = v.object({
+  type: v.literal("reasoning"),
+  reasoning: v.string(),
+});
+
+const ToolInvocation = v.union(
+  v.object({
+    state: v.literal("partial-call"),
+    toolCallId: v.string(),
+    toolName: v.string(),
+    args: v.any(),
+  }),
+  v.object({
+    state: v.literal("call"),
+    toolCallId: v.string(),
+    toolName: v.string(),
+    args: v.any(),
+  }),
+  v.object({
+    state: v.literal("result"),
+    toolCallId: v.string(),
+    toolName: v.string(),
+    args: v.any(),
+    result: v.any(),
+  }),
+);
+
+const ToolInvocationUIPart = v.object({
+  type: v.literal("tool-invocation"),
+  toolInvocation: ToolInvocation,
+});
+
+const Source = v.object({
+  sourceType: v.literal("url"),
+  id: v.string(),
+  url: v.string(),
+  title: v.optional(v.string()),
+});
+
+const SourceUIPart = v.object({
+  type: v.literal("source"),
+  source: Source,
+});
+
+const StepStartUIPart = v.object({
+  type: v.literal("step-start"),
+});
+
+const Attachment = v.object({
+  name: v.optional(v.string()),
+  contentType: v.optional(v.string()),
+  url: v.string(),
+});
+
+const UIMessage = v.object({
+  role: v.union(
+    v.literal("system"),
+    v.literal("user"),
+    v.literal("assistant"),
+    v.literal("data"),
+  ),
+  createdAt: v.optional(v.number()),
+  content: v.string(),
+  annotations: v.optional(v.array(v.any())),
+  parts: v.array(
+    v.union(
+      TextUIPart,
+      ReasoningUIPart,
+      ToolInvocationUIPart,
+      SourceUIPart,
+      StepStartUIPart,
+    ),
+  ),
+  experimental_attachements: v.optional(v.array(Attachment)),
+});
 
 const schema = defineSchema({
   ...authTables,
+  chats: defineTable({
+    id: v.string(),
+    name: v.string(),
+    pinned: v.boolean(),
+    updatedAt: v.number(),
+    model: v.string(),
+    branchedFrom: v.optional(v.id("chats")),
+    user: v.id("users"),
+  }),
+  messages: defineTable({
+    ...UIMessage.fields,
+    model: v.string(),
+    chat: v.id("chats"),
+  }),
 });
 
 export default schema;
