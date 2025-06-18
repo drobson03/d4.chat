f6406ded5b4468f80f4c6bdc5fb3223f10948b14
Darcy Robson <17976794+drobson03@users.noreply.github.com>
2025-06-18 15:56:58 +1000

    chore: nuke index

--- src/routes/_authed.index.tsx
+++ src/routes/_authed.index.tsx
@@ -1,13 +1,7 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_authed/")({
-  component: RouteComponent,
+  beforeLoad: async () => {
+    throw redirect({ to: "/chat" });
+  },
 });
-
-function RouteComponent() {
-  return (
-    <main>
-      <h1 className="text-3xl text-blue-500 mb-5">D4 Chat</h1>
-    </main>
-  );
-}
