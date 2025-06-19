7f8fff54d11e02c9ce82f6301d3bd38f57739edd
Darcy Robson <17976794+drobson03@users.noreply.github.com>
2025-06-19 18:19:10 +1000

    fix: don't check for window in useSyncExternalStore

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -3,27 +3,19 @@ import { useSyncExternalStore } from "react";
 const MOBILE_BREAKPOINT = 768;
 
 function subscribe(callback: () => void) {
-  if (typeof window === "undefined") {
-    return () => {};
-  }
-
   const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
   mql.addEventListener("change", callback);
   return () => {
-    window.removeEventListener("change", callback);
+    mql.removeEventListener("change", callback);
   };
 }
 
-function getSnapshot() {
-  if (typeof window === "undefined") {
-    return false;
-  }
-
-  return window.innerWidth < MOBILE_BREAKPOINT;
-}
-
 export function useIsMobile() {
-  const isMobile = useSyncExternalStore(subscribe, getSnapshot, () => false);
+  const isMobile = useSyncExternalStore(
+    subscribe,
+    () => window.innerWidth < MOBILE_BREAKPOINT,
+    () => false,
+  );
 
   return isMobile;
 }
